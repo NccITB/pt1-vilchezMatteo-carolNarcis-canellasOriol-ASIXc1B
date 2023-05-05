178c22d9cb8a92b3de0429311cc0a98b1f07b764
sjo <oriol.canellas.7e3@itb.cat>
2023-05-05 12:08:19 +0200

    ocb

---
+++ probes/reloj_v3prova.js
@@ -0,0 +1,18 @@
+actualitzaRellotja()
+function actualitzaRellotja(){
+        momentActual = new Date()
+        hora = momentActual.getHours()
+        minut = momentActual.getMinutes()
+        segons = momentActual.getSeconds()
+
+        if (hora < 10) hora = "0" + hora
+        if (minut < 10) minut = "0" + minut
+        if (segons < 10) segons = "0" + segons
+
+        horaImprimible = hora + " : " + minut + " : " segons
+
+        document.title = horaImprimible
+        horaActual.innerHTML= horaImprimible
+
+        setTimeout("actualitzaRellotja()",1000)
+}
